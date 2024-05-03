fe425ac3233a0b0825439d38224bc747efc30afe
lj <janluethi2000@gmail.com>
2024-05-03 11:35:27 +0200

    preprocess papers

---
+++ preprocess_paper.py
@@ -0,0 +1,82 @@
+import re
+import os
+
+# Not used, but may be useful to check whether all affiliations are removed
+def find_affiliations(text):
+    # Pattern for matching LaTeX affiliation-related commands
+    patterns = [
+        r'\\affiliation[\[\d+\]]?\{([^}]*)\}',
+        r'\\affil[\[\d+\]]?\{([^}]*)\}',
+        r'\\address(?:\[.*?\])?\{([^}]*)\}',
+        r'\\institute[\[.*?\]]?\{([^}]*)\}'
+    ]
+    
+    matches = []  # List to store all matches
+
+    # Iterate over each pattern and find all matches in the text
+    for pattern in patterns:
+        matches.extend(re.findall(pattern, text))
+    
+    return matches
+
+def remove_all_comments(text):
+    text = '\n'.join([line for line in text.split('\n') if not line.strip().startswith('%')])
+    return re.sub(r'%.*\n', '', text)
+
+def remove_thanks(text):
+    return re.sub(r'\\thanks\{[^}]*\}', '', text)
+
+def get_title(text):
+    title = re.search(r'\\title\{([^}]*)\}', text)
+    return title.group(1) if title else None
+
+def get_abstract(text):
+    abstract = re.search(r'\\begin\{abstract\}([\s\S]*)\\end\{abstract\}', text)
+    return abstract.group(1) if abstract else None
+
+def get_make_title_position(text: str):
+    return text.find('\\maketitle')
+
+def remove_before_make_title(text):
+    return re.sub(r'[^]*\\maketitle', '', text)
+
+def prepend_abstract(text, abstract):
+    return f'\\begin{{abstract}}{abstract}\\end{{abstract}}\n{text}'
+
+def prepend_title(text, title):
+    return f'\\title{{{title}}}\n{text}'
+
+def prepend_affiliation(text, keyword_affiliation):
+    return f'\\affiliation{{{keyword_affiliation}}}\n{text}'
+
+def prepend_begin_document(text):
+    return f'\\begin{{document}}\n{text}'
+
+def preprocess(tex_file, keyword_affiliation = '$$_affiliation_$$'):
+    print(f"Preprocessing {os.path.basename(tex_file)}: ")
+    with open(tex_file, 'r') as f:
+        tex_source = f.read()
+
+    tex_source = remove_all_comments(tex_source)
+    # some people place thanks in title, so we remove it first
+    tex_source = remove_thanks(tex_source)
+    title = get_title(tex_source)
+    abstract = get_abstract(tex_source)
+    make_title_pos = get_make_title_position(tex_source)
+
+    if title is None or abstract is None or make_title_pos == -1:
+        return False
+    
+    # Remove everything before \maketitle
+    tex_source = tex_source[make_title_pos:]
+
+    if tex_source.find('\\begin{abstract}') == -1:
+        tex_source = prepend_abstract(tex_source, abstract)
+
+    tex_source = prepend_title(tex_source, title)
+    tex_source = prepend_affiliation(tex_source, keyword_affiliation)
+    tex_source = prepend_begin_document(tex_source)
+
+    with open(tex_file, 'w') as f:
+        f.write(tex_source)
+    return True
